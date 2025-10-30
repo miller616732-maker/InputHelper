d1094896e0f2f79d489e8487c9ac861c1ea64536
616732 <miller.616732@swl.k12.oh.us>
2025-10-30 08:55:21 -0400

    message

--- src/InputHelper.java
+++ src/InputHelper.java
@@ -1,5 +1,56 @@
-public class Main {
-    public static void main(String[] args) {
-        
+import java.util.Scanner;
+public class InputHelper {
+    //getNonZeroLenString returns a String of non-zero length
+    public static String getNonZeroLenString(Scanner scan, String prompt) {
+        boolean valid = false;
+        String userInput;
+        System.out.println(prompt);
+        do {
+            userInput = scan.nextLine();
+            if (!userInput.isEmpty()) {
+                valid = true;
+            } else {
+                System.out.println("Error: Enter a string of non-zero length");
+            }
+        } while (!valid);
+        return userInput;
+    }
+
+    //The getInt method returns a valid int value back to the user
+    public static int getInt(Scanner scan, String prompt) {
+        boolean valid = false;
+        int userInput = 0;
+        System.out.println(prompt);
+        do {
+            if (scan.hasNextInt()) {
+                userInput = scan.nextInt();
+                valid = true;
+            } else {
+                System.out.println("Error: Enter an integer value");
+                scan.nextLine();
+            }
+        } while (!valid);
+        return userInput;
+    }
+    public static int getRangedInt(Scanner scan, String prompt, int min, int max) {
+        boolean valid = false;
+        int userInput = 0;
+        System.out.println(prompt);
+        do {
+            if (scan.hasNextInt()) {
+                userInput = scan.nextInt();
+                if (userInput >= min && userInput <= max) {
+                    valid = true;
+                } else {
+                    System.out.println("Error: Input must be between " + min + " and " + max + ".");
+                    scan.nextLine();
+                }
+            }
+            else {
+                System.out.println("Error: Enter an integer value");
+                scan.nextLine();
+            }
+        } while (!valid);
+        return userInput;
     }
 }
--- src/MainTest.java
+++ src/MainTest.java
@@ -1,4 +1,12 @@
-package PACKAGE_NAME;
-
+import java.util.Scanner;
 public class MainTest {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String name = InputHelper.getNonZeroLenString(scan, "Enter your name");
+        System.out.println(name);
+        int favoriteNumber = InputHelper.getInt(scan, "What is your favorite number?");
+        System.out.println(favoriteNumber);
+        int numGuess = InputHelper.getRangedInt(scan, "Guess a number between 1-10", 1, 10);
+        System.out.println("You guessed " + numGuess);
+    }
 }
